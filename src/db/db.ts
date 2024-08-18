b6d0352056cb0cbd651a95ca49b63f312ed297b4
enthusiast coder <139620395+Rajankannaujiya@users.noreply.github.com>
2024-08-18 10:49:36 +0530

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,11 +5,8 @@ dotenv.config();
 
 
 const db = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port:parseInt(process.env.DB_PORT || '5432')
+    connectionString:process.env.DATABASE_URI
   });
 
+  db.connect();
   export default db;
