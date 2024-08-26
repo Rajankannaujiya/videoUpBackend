67ae18607fefeccd4a81ca6691c6acf4c2e9933e
enthusiast coder <139620395+Rajankannaujiya@users.noreply.github.com>
2024-08-26 22:34:27 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import express from 'express'
 import cors from 'cors'
+import compression from 'compression'
 
 import { createImageTable, createUsersTable, createVideoTable} from './db/table';
 
